485174befc5d665f6f2673658a89ad30cb43c439
1ktoto1 <43352108+1ktoto1@users.noreply.github.com>
2020-04-20 01:32:16 +0300

    Update create.sql

--- create.sql
+++ create.sql
@@ -9,14 +9,14 @@ CREATE TABLE Game(
 CREATE TABLE Author(
     author_name VARCHAR(50) NOT NULL PRIMARY KEY,
     author_country VARCHAR(25) NOT NULL,
-    author_url VARCHAR(255),
+    author_url VARCHAR(256),
     FOREIGN KEY(author_country) REFERENCES Country (land)
 );
 
 CREATE TABLE Map (
     map_id INT DEFAULT 0 CHECK(map_id >= 0) PRIMARY KEY,
     author_name VARCHAR(50) NOT NULL,
-    map_name VARCHAR(70) NOT NULL,
+    map_name VARCHAR(256) NOT NULL,
     map_date DATE NOT NULL,
     rating INT DEFAULT 0,
     map_game VARCHAR(50) NOT NULL,
