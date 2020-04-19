c7a0e9285c0605559a93fabcc1eb82255365adae
1ktoto1 <43352108+1ktoto1@users.noreply.github.com>
2020-04-20 00:11:06 +0300

    Create populate.sql

---
+++ populate.sql
@@ -0,0 +1,63 @@
+INSERT INTO Country(land)
+VALUES('Италия');
+INSERT INTO Country(land)
+VALUES('Финляндия');
+INSERT INTO Country(land)
+VALUES('Норвегия');
+INSERT INTO Country(land)
+VALUES('Дания');
+INSERT INTO Country(land)
+VALUES('Россия');
+INSERT INTO Country(land)
+VALUES('Украина');
+INSERT INTO Country(land)
+VALUES('США');
+INSERT INTO Country(land)
+VALUES('Корея');
+
+INSERT INTO game(play)
+VALUES('Герои Меча и Магии III: Возрождение Эрафии');
+INSERT INTO game(play)
+VALUES('Герои Меча и Магии III: Во Имя Богов');
+
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Antti Karhu','Финляндия','http://heroesportal.net/profile.php?id=588');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Stian Olsen','Норвегия','	http://heroesportal.net/profile.php?id=590');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Morten','Дания','http://heroesportal.net/profile.php?id=582');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Rocco D Giordano','Италия','http://heroesportal.net/profile.php?id=586');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('C Mojo','Корея','http://heroesportal.net/profile.php?id=574');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Never','Россия','	http://heroesportal.net/profile.php?id=573');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Kian Colestock','США','	http://heroesportal.net/profile.php?id=577');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Gorbik','Украина','http://heroesportal.net/profile.php?id=11');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('il','Украина','http://heroesportal.net/profile.php?id=587');
+INSERT INTO Author(author_name, author_country, authot_url)
+VALUES('Matteo Carriero','Италия','http://heroesportal.net/profile.php?id=572');
+
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Antti Karhu','The Quest',2002-05-13,12,'Герои Меча и Магии III: Возрождение Эрафии',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Stian Olsen','Winter Wars',2004-05-13,52,'Герои Меча и Магии III: Возрождение Эрафии',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Morten','The Quest',2006-07-15,22,'Герои Меча и Магии III: Возрождение Эрафии',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Rocco D Giordano','The Quest',2011-03-02,2,'Герои Меча и Магии III: Возрождение Эрафии',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'C Mojo','The Quest',2002-05-13,67,'Герои Меча и Магии III: Во Имя Богов',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Never','The Quest',2002-05-13,14,'Герои Меча и Магии III: Во Имя Богов',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Kian Colestock','The Quest',2004-05-13,62,'Герои Меча и Магии III: Во Имя Богов',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Antti Karhu','The Quest',2006-05-13,12,'Герои Меча и Магии III: Во Имя Богов',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Kian Colestock','The Quest',2001-05-13,22,'Герои Меча и Магии III: Во Имя Богов',64543);
+INSERT INTO Map(map_id, author_name, map_name, map_date, rating, map_game, dowloads)
+VALUES(1,'Never','The Quest',2002-05-13,12,'Герои Меча и Магии III: Во Имя Богов',64543);
